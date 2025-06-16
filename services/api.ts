fb34e05ee5648f95f8438a34506ce10e0c4aec07
Tanjid Karim Shafin <bifaem5@gmail.com>
2025-06-16 23:48:35 +0600

    API_BASE_URL changed

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-const API_BASE_URL = 'https://foodiehub-backend-zs3s.onrender.com/api';
+const API_BASE_URL = 'https://foodiehub-backend-zs3s.onrender.com';
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
